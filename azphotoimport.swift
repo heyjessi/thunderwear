c230ebbef63b3bdf89d254711fb6c7d56ac1a4d0
Amy Zhou <amy_zhou@college.harvard.edu>
2017-11-30 21:13:17 -0500

    really basic photoimport//very subject to change

--- azphotoimport.swift
+++ azphotoimport.swift
@@ -1,1 +1,38 @@
+//https://www.youtube.com/watch?v=v8r_wD_P3B8
+//remember to add message for camera
+
 import UIKit
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBAction func importImage(_ sender: AnyObject)
+    {
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        image.sourceType = UIImagePickerControllerSourceType.Camera
+        
+        image.allowsEditing = false
+        
+        self.present(image, animated: true)
+        {
+            //After it's complete
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
+    {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            myImageView.image = image
+        }
+        else
+        {
+            //Error message
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+}

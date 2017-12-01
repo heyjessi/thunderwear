75418a8abfb3f3dcc175e3972724be565dc3caa3
Amy Zhou <amy_zhou@college.harvard.edu>
2017-12-01 03:53:02 -0500

    change

--- azlabelcheckbox.swift
+++ azlabelcheckbox.swift
@@ -1,1 +1,18 @@
+//taken from stackoverflowfrom
+//https://stackoverflow.com/questions/29117759/how-to-create-radio-buttons-and-checkbox-in-swift-ios
 
+//var radioButtonController = SSRadioButtonsController()
+//radioButtonController.setButtonsArray([button1!,button2!,button3!])
+
+var unchecked = true
+
+@IBAction func tick(sender: UIButton) {
+    if unchecked {
+        sender.setImage(UIImage(named:"checked.png"), forControlState: .Normal)
+        unchecked = false
+    }
+    else {
+        sender.setImage( UIImage(named:"unchecked.png"), forControlState: .Normal)
+        unchecked = true
+    }
+}
